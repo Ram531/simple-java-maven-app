d3ec91fa9223f43ff9f0904b9314c07e8e960f69
Ram N <rganesh333@gmail.com>
2019-02-28 21:59:38 +0530

    Update App.java
    Added the display message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World to Ram Ganesh N!";
 
     public App() {}
 
