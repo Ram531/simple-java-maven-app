53a478861089c8f5119a398170b0d44bc643df14
Ram N <rganesh333@gmail.com>
2019-02-28 22:20:37 +0530

    Update App.java for commit message
    Update the file for a display message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World to Ram Ganesh N and Jenkins. Nice commit message.";
+    private final String message = "Hello World to Ram Ganesh N and Jenkins for Poll SCM. Nice commit message.";
 
     public App() {}
 
